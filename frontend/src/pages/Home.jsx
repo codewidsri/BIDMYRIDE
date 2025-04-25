b065efb296f58dccdb6ebb1cd396a86de2688edb
codewidsri <142038601+codewidsri@users.noreply.github.com>
2025-04-25 14:22:39 +0000

    second commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,9 @@
-import { Container, Nav, Navbar } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import Header from "../components/Header"
 
 function Home() {
     return (
         <>
-            <Navbar className="bg-dark">
-                <Container>
-                    <Navbar.Brand href="#"><Link to='/' className="text-decoration-none fw-bold">BIDMYRIDE</Link></Navbar.Brand>
-                    <Nav>
-                        <Nav.Item>
-                            <Link to='/' className="text-decoration-none text-white me-3 nav-hover">Home</Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Link to='/aboutus' className="text-decoration-none text-white me-3 nav-hover">About Us</Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Link to='/contactus' className="text-decoration-none text-white me-3 nav-hover">Contact Us</Link>
-                        </Nav.Item>
-                        <Nav.Item className="">
-                            <Link to='/login' className="text-decoration-none text-white me-3 bg-primary rounded px-2 py-2">Login/Register</Link>
-                        </Nav.Item>
-                    </Nav>
-                </Container>
-            </Navbar>
+            <Header />  
         </>
     )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,44 @@
-import { Container, Row, Col } from 'react-bootstrap'
-
-function Login() {
-    return
-    <>
-    </>
+import {Container,Typography,Box,Button,Paper,TextField, FormControlLabel, Checkbox } from '@mui/material'
+import { useState } from 'react';
+function Login(){
+    const [form,setform] = useState({email : '', password : ''})
+    const [showpassword,setshowpassword]=useState(false)
+    function HandleChange(e){
+        setform({...form,[e.target.name]:e.target.value})
+    }
+    function handleSubmit(e){
+        e.preventDefault()
+    }
+    return (<>
+        <Container maxWidth="xs">
+            <Paper elevation={3} sx={{ p: 4, mt: 15 }}>
+                <Typography variant='h5' gutterBottom align='center' sx={{fontWeight: "bold",textTransform: 'uppercase'}}>Login</Typography>
+                <Box component='form' onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                    <TextField 
+                        label='Email' 
+                        name='email' 
+                        type='email' 
+                        required 
+                        fullWidth 
+                        margin='normal' 
+                        value={form.email} 
+                        onChange={HandleChange}
+                    />
+                    <TextField
+                        label='Passoword'
+                        name='password'
+                        type={showpassword ? 'text' : 'password'}
+                        required
+                        fullWidth
+                        margin='normal'
+                        value={form.password}
+                        onChange={HandleChange}
+                    />
+                     <FormControlLabel control={<Checkbox onChange={()=>setshowpassword(!showpassword)} />} label="show password" />
+                    <Button type='submit' variant='contained' fullWidth>submit</Button>
+                </Box>
+            </Paper>
+        </Container>
+    </>)
 }
-
 export default Login;
